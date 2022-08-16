aaf13d7f515b50551ca8243c34ebae15b00326f6
Danny Jose Jimenez Gutierrez <dennaly88@gmail.com>
2022-08-16 18:33:25 -0400

    CODIGO HTM

---
+++ index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+<link rel="icon" href="#">
+<title>MAQUILLAJE</title>
+<link rel="shortcut icon" href="img/favico.ico">
+<link href="css/style.css" rel="stylesheet">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<link href='https://fonts.googleapis.com/css?family=Great+Vibes' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Oswald:400,700,300' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:400,400italic,300italic,300,700,700italic' rel='stylesheet' type='text/css'>
+
+</head>
+<body class="frontpage">
+
+<div id="loader-wrapper">
+<div id="loader"></div>
+<div class="loader-section section-left"></div>
+<div class="loader-section section-right"></div>
+</div>
+
+
+<div class="menu-outer">
+    <div class="menu-icon">
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+    </div>
+    <nav>
+        <ul>
+           <li><a href="#">Principal</a></li>
+           <li><a href="#">MAQUILLAJE</a></li>
+           <li><a target="_blank" href="#">ARTICULOS</a></li>
+       </ul>
+   </nav>
+</div>
+<a class="menu-close" onClick="return true">
+    <div class="menu-icon">
+        <div class="bar"></div>
+        <div class="bar"></div>
+    </div>
+</a>
+
+<div class="fixedcallicon">
+	<i class="fa fa-phone"></i><span class="hide">Book Now - 897.645.223</span>
+</div>
+<div class="container">
+	<section class="background">
+	<div class="content-wrapper">
+		<div class="intro">
+			<h1><span class="smaller wow zoomIn" data-wow-duration="2s" data-wow-delay="0.5">ASI SOMOS</span>
+			<span class="big wow pulse" data-wow-duration="1s" data-wow-delay="0s"><a href="S.I.G.A.I/index.php">
+			</a>Maquillaje </span><br/>
+			<span class="small wow fadeIn" data-wow-duration="2s" data-wow-delay="0.5s"><img src="img/logo.png" ></span>
+			</h1>
+		</div>
+	</div>
+	</section>
+	<section class="background">
+	<div class="content-wrapper">
+		<div class="about">
+			<div class="aboutbadge">
+				<span><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><br/>
+				<span class="border">GERENCIA DE IMAGEN </span><br/>
+				Maquillaje y Vestuario</span>
+			</div>
+			<div class="aboutbadge black">
+				<span>Maquillaje hace mención al acto y el resultado de maquillarse o de maquillar: es decir, de apelar a
+				 productos cosméticos para cambiar la apariencia o embellecer el 
+				 rostro u otra parte del cuerpo. También se llama maquillaje a la sustancia que se utiliza para tal fin.</span>
+			</div>
+		</div>
+	</div>
+	</section>
+	<section class="background">
+	<div class="content-wrapper">
+		<div class="voucher">
+			<div class="voucher-whitetransparent">
+				<h2><img src="img/logo.png" ><br/>Personal Especializado <br/>Cortes y Secado<br/><a href="#slide6">Imagen</a></h2>
+			</div>
+		</div>
+	</div>
+	</section>
+	<section class="background">
+	<div class="content-wrapper">
+		<div class="pricingbadge">
+			<h4><img src="img/logo.png" ></h4>
+			        Personal Encargado <br><br>
+					 Rafael guzman 
+					 Jefe de Division 
+					 EXt 1287
+					 Telefono 04125588784
+<br><br>
+					 Marelis Aponte 
+					 Coordinador Encargado
+					 EXT 1512
+					 Telefono  04142257878
+		</div>
+	</div>
+	</section>
+	<section class="background">
+	<div class="content-wrapper">
+		<div class="testimonialarea">
+			<div class="testimonialarea-bubble">
+				<div class="testimonial-widget">
+					<img src="img/logo.png" ></span>
+					<h3 class="uppercase">Nos Encontramos en </h3>
+					<div class="testimonial">
+						<p>
+							 "PLANTA BAJA CERCA DEL ESTUDIO GIGANTE AL LADO DEL CAJERO AUTOMATICO 
+							 TELEFONOS 04142334545 - 04125454445
+							 "
+						</p>
+						
+					</div>
+					
+					
+					<button class="prev-testimonial">Prev</button>
+					<button class="next-testimonial">Next</button>
+				</div>
+			</div>
+			
+			
+		</div>
+	
+</div>
+
+
+	</section>
+</div>
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="js/common.js"></script>
+<script src="js/home.js"></script>
+<script src="js/testimonials.js"></script>
+
+</body>
+</html>
